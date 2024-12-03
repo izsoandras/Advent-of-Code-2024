6b6d621e98f3d73dfac771ebe03f474531db4b55
andras <Andras Izso>
2024-12-03 23:41:06 +0100

    Update solve.bash, to accept input from outside, instead of predefined

--- day3/solve.bash
+++ day3/solve.bash
@@ -1,1 +1,1 @@
-cat input | sed -e 's/mul/\nmul/g' -e 's/)/)\n/g' | grep 'mul([0-9][0-9]*,[0-9][0-9]*)' | ./mul_add
+sed -e 's/mul/\nmul/g' -e 's/)/)\n/g' | grep 'mul([0-9][0-9]*,[0-9][0-9]*)' | ./mul_add
